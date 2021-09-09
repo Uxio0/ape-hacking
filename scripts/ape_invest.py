e5dd29cbae23f8120f27e80dbaf5cc03e24b61cd
Uxio Fuentefria <uxio@gnosis.pm>
2021-09-09 19:33:14 +0200

    Add CI

--- scripts/ape_invest.py
+++ scripts/ape_invest.py
@@ -1,28 +1,27 @@
 from binascii import Error
-
+from eth_account import Account
+from ape_safe import ApeSafe
 from os import environ
 
 
-
 def main():
-    from eth_account import Account
-    from ape_safe import ApeSafe
     private_keys = environ.get('PRIVATE_KEY')  # Private keys separated by ,
     sender_key = environ.get('PRIVATE_SENDER_KEY')  # Private key
+    safe_address = environ.get('SAFE_ADDRESS', 'ychad.eth')
 
     try:
-        owner_accounts = [Account.from_key(private_key) for private_key in private_keys.split(',')]
+        owner_accounts = [Account.from_key(private_key) for private_key in private_keys.split(',')] if private_keys else []
     except (ValueError, Error):
         print('Not valid private keys on $PRIVATE_KEY')
         raise
 
     try:
-        sender_account = Account.from_key(sender_key)
+        sender_account = Account.from_key(sender_key) if sender_key else None
     except (ValueError, Error):
         print('Not valid private keys on $PRIVATE_KEY')
         raise
 
-    safe = ApeSafe('ychad.eth')
+    safe = ApeSafe(safe_address)
 
     dai = safe.contract('0x6B175474E89094C44Da98b954EedeAC495271d0F')
     vault = safe.contract('0x19D3364A399d251E894aC732651be8B0E4e85001')
